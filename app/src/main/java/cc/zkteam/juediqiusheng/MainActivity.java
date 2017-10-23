f831edd131fb0d816c59e4fa7a93991d89d63749
qingw <qingw@jumei.com>
2017-10-23 10:32:32 +0800

    初始化 绝地求生 项目。

---
+++ app/src/main/java/cc/zkteam/juediqiusheng/MainActivity.java
@@ -0,0 +1,13 @@
+package cc.zkteam.juediqiusheng;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
