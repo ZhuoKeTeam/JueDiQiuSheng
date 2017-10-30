c0330589307fcdef3e1ebeeef6be037d5cf2e9ff
qingw <qingw@jumei.com>
2017-10-30 13:31:25 +0800

    1. 添加对应的 Fragment。

--- app/src/main/java/cc/zkteam/juediqiusheng/activity/MainActivity.java
+++ app/src/main/java/cc/zkteam/juediqiusheng/activity/MainActivity.java
@@ -7,10 +7,16 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.view.ViewGroup;
 import android.widget.TextView;
 
 import com.alibaba.android.arouter.launcher.ARouter;
@@ -29,7 +35,54 @@ public class MainActivity extends BaseActivity {
 
     public static final String TAG = "MainActivity";
 
+    /**
+     * The {@link android.support.v4.view.PagerAdapter} that will provide
+     * fragments for each of the sections. We use a
+     * {@link FragmentPagerAdapter} derivative, which will keep every
+     * loaded fragment in memory. If this becomes too memory intensive, it
+     * may be best to switch to a
+     * {@link android.support.v4.app.FragmentStatePagerAdapter}.
+     */
+    private SectionsPagerAdapter mSectionsPagerAdapter;
+    /**
+     * The {@link ViewPager} that will host the section contents.
+     */
+    private ViewPager mViewPager;
+    private BottomNavigationView navigation;
     private TextView mTextMessage;
+
+    private ViewPager.OnPageChangeListener onPageChangeListener = new ViewPager.OnPageChangeListener() {
+        @Override
+        public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+        }
+
+        @Override
+        public void onPageSelected(int position) {
+            int itemId = R.id.navigation_recommend;
+            switch (position) {
+                case 0:
+                    itemId = R.id.navigation_recommend;
+                    break;
+                case 1:
+                    itemId = R.id.navigation_game;
+                    break;
+                case 2:
+                    itemId = R.id.navigation_picture;
+                    break;
+                case 3:
+                    itemId = R.id.navigation_question;
+                    break;
+            }
+
+            navigation.setSelectedItemId(itemId);
+        }
+
+        @Override
+        public void onPageScrollStateChanged(int state) {
+
+        }
+    };
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -38,15 +91,19 @@ public class MainActivity extends BaseActivity {
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
             switch (item.getItemId()) {
                 case R.id.navigation_recommend:
+                    mViewPager.setCurrentItem(0);
                     mTextMessage.setText(R.string.title_recommend);
                     return true;
                 case R.id.navigation_game:
+                    mViewPager.setCurrentItem(1);
                     mTextMessage.setText(R.string.title_game);
                     return true;
                 case R.id.navigation_picture:
+                    mViewPager.setCurrentItem(2);
                     mTextMessage.setText(R.string.title_picture);
                     return true;
                 case R.id.navigation_question:
+                    mViewPager.setCurrentItem(3);
                     mTextMessage.setText(R.string.title_question);
                     return true;
             }
@@ -59,10 +116,24 @@ public class MainActivity extends BaseActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
+        // Create the adapter that will return a fragment for each of the three
+        // primary sections of the activity.
+        mSectionsPagerAdapter = new SectionsPagerAdapter(getSupportFragmentManager());
+
+
         TextView textView = (TextView) findViewById(R.id.jump);
         mTextMessage = (TextView) findViewById(R.id.message);
-        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+
+        // Set up the ViewPager with the sections adapter.
+        mViewPager = (ViewPager) findViewById(R.id.container);
+
+        mViewPager.addOnPageChangeListener(onPageChangeListener);
+
+        mViewPager.setAdapter(mSectionsPagerAdapter);
+
 
         textView.setOnClickListener(new OnClickListener() {
             @Override
@@ -79,6 +150,12 @@ public class MainActivity extends BaseActivity {
 
         // 演示 如何使用 LifeComponents
         testLifeComponents(textView);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        mViewPager.removeOnPageChangeListener(onPageChangeListener);
     }
 
     /**
@@ -137,5 +214,65 @@ public class MainActivity extends BaseActivity {
                 }
             }
         });
+    }
+
+
+    /**
+     * A placeholder fragment containing a simple view.
+     */
+    public static class PlaceholderFragment extends Fragment {
+        /**
+         * The fragment argument representing the section number for this
+         * fragment.
+         */
+        private static final String ARG_SECTION_NUMBER = "section_number";
+
+        public PlaceholderFragment() {
+        }
+
+        /**
+         * Returns a new instance of this fragment for the given section
+         * number.
+         */
+        public static PlaceholderFragment newInstance(int sectionNumber) {
+            PlaceholderFragment fragment = new PlaceholderFragment();
+            Bundle args = new Bundle();
+            args.putInt(ARG_SECTION_NUMBER, sectionNumber);
+            fragment.setArguments(args);
+            return fragment;
+        }
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                                 Bundle savedInstanceState) {
+            View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+            TextView textView = (TextView) rootView.findViewById(R.id.section_label);
+            textView.setText(getString(R.string.section_format, getArguments().getInt(ARG_SECTION_NUMBER)));
+            return rootView;
+        }
+    }
+
+    /**
+     * A {@link FragmentPagerAdapter} that returns a fragment corresponding to
+     * one of the sections/tabs/pages.
+     */
+    public class SectionsPagerAdapter extends FragmentPagerAdapter {
+
+        public SectionsPagerAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            // getItem is called to instantiate the fragment for the given page.
+            // Return a PlaceholderFragment (defined as a static inner class below).
+            return PlaceholderFragment.newInstance(position + 1);
+        }
+
+        @Override
+        public int getCount() {
+            // Show 3 total pages.
+            return 4;
+        }
     }
 }
