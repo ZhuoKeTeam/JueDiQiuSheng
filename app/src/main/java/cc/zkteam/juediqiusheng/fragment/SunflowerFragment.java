81072b5997c5fd5cf7bf3d4182b732b21e7d2312
lvwenbo <443818654@qq.com>
2017-11-06 17:13:56 +0800

    设计葵花宝典页面 以及点击跳转网页

--- app/src/main/java/cc/zkteam/juediqiusheng/fragment/SunflowerFragment.java
+++ app/src/main/java/cc/zkteam/juediqiusheng/fragment/SunflowerFragment.java
@@ -1,13 +1,28 @@
 package cc.zkteam.juediqiusheng.fragment;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import cc.zkteam.juediqiusheng.R;
+import cc.zkteam.juediqiusheng.activity.SortDetailActivity;
+import cc.zkteam.juediqiusheng.bean.RecommendedBean;
+import cc.zkteam.juediqiusheng.managers.ZKConnectionManager;
+import cc.zkteam.juediqiusheng.retrofit2.ZKCallback;
+import cc.zkteam.juediqiusheng.strategy.CommonAdapter;
+import cc.zkteam.juediqiusheng.strategy.base.ViewHolder;
+import cc.zkteam.juediqiusheng.strategy.wrapper.HeaderAndFooterWrapper;
+import cc.zkteam.juediqiusheng.strategy.wrapper.LoadMoreWrapper;
 
 /**
  * Created by WangQing on 2017/10/30.
@@ -19,7 +34,11 @@ public class SunflowerFragment extends Fragment {
      * fragment.
      */
     private static final String ARG_SECTION_NUMBER = "section_number";
-
+    private RecyclerView mRecyclerView;
+    private List<RecommendedBean> mDatas = new ArrayList<>();
+    private CommonAdapter<RecommendedBean> mAdapter;
+    private HeaderAndFooterWrapper mHeaderAndFooterWrapper;
+    private LoadMoreWrapper mLoadMoreWrapper;
     public SunflowerFragment() {
     }
 
@@ -39,8 +58,71 @@ public class SunflowerFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_rec_sunflower, container, false);
-        TextView textView = (TextView) rootView.findViewById(R.id.text);
-        textView.setText(getString(R.string.section_format, getArguments().getString(ARG_SECTION_NUMBER)));
+        mRecyclerView= (RecyclerView) rootView.findViewById(R.id.sunflower_main_list);
+        mRecyclerView.setHasFixedSize(true);
+        mRecyclerView.setLayoutManager(new GridLayoutManager(getActivity(), 1));
+        mAdapter = new CommonAdapter<RecommendedBean>(getActivity(), R.layout.list_item, mDatas) {
+            @Override
+            protected void convert(ViewHolder holder, RecommendedBean r, int position) {
+                holder.setText(R.id.text, r.getContext());
+            }
+        };
+        initHeaderAndFooter();
+        mLoadMoreWrapper = new LoadMoreWrapper(mHeaderAndFooterWrapper);
+//        mLoadMoreWrapper.setLoadMoreView(R.layout.default_loading);
+        initDatas();
+        mRecyclerView.setAdapter(mLoadMoreWrapper);
+        mAdapter.setOnItemClickListener(new CommonAdapter.OnItemClickListener() {
+            @Override
+            public void onItemClick(View view, RecyclerView.ViewHolder holder, int position) {
+                Intent intent = new Intent();
+                intent.setClass(getActivity(), SortDetailActivity.class);
+                String artifactUrl = mDatas.get(position).getNewsSourceUrl();
+                String url = artifactUrl.substring(artifactUrl.lastIndexOf("\">") + 2, artifactUrl.lastIndexOf("</a>"));
+                intent.putExtra("url", url);
+                startActivity(intent);
+            }
+
+            @Override
+            public boolean onItemLongClick(View view, RecyclerView.ViewHolder holder, int position) {
+                return false;
+            }
+        });
+
         return rootView;
+    }
+
+
+    private void initHeaderAndFooter() {
+        mHeaderAndFooterWrapper = new HeaderAndFooterWrapper(mAdapter);
+        ImageView imageView = new ImageView(getActivity());
+        imageView.setImageResource(R.mipmap.ic_launcher);
+        imageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getActivity().getApplicationContext(), "轮播图", Toast.LENGTH_SHORT).show();
+            }
+        });
+        mHeaderAndFooterWrapper.addHeaderView(imageView);
+    }
+
+
+    /**
+     * 演示快速使用测试 Api
+     */
+    private void initDatas() {
+        ZKConnectionManager.getInstance().getZKApi().getRecommended("10002","20")
+                .enqueue(new ZKCallback<List<RecommendedBean>>() {
+                    @Override
+                    public void onResponse(List<RecommendedBean> result) {
+                        mDatas.clear();
+                        mDatas.addAll(result);
+                        mLoadMoreWrapper.notifyDataSetChanged();
+                    }
+
+                    @Override
+                    public void onFailure(Throwable throwable) {
+                    }
+                });
     }
 }
