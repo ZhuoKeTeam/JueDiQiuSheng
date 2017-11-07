9cc58f834fa561e79c4ddf7442b93b854efc63c8
qingw <qingw@jumei.com>
2017-11-07 20:34:03 +0800

    1. 修复首页的一些问题。

--- app/src/main/java/cc/zkteam/juediqiusheng/activity/MainActivity.java
+++ app/src/main/java/cc/zkteam/juediqiusheng/activity/MainActivity.java
@@ -261,8 +261,6 @@ public class MainActivity extends BaseActivity {
         public View onCreateView(LayoutInflater inflater, ViewGroup container,
                                  Bundle savedInstanceState) {
             View rootView = inflater.inflate(R.layout.fragment_main, container, false);
-            TextView textView = (TextView) rootView.findViewById(R.id.section_label);
-            textView.setText(getString(R.string.section_format, getArguments().getString(ARG_SECTION_NUMBER)));
             return rootView;
         }
 
