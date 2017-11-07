1632bce81dd8114f3bd1f110a04735bd93bfbbf0
renxuelong <renxuelongvip@163.com>
2017-11-07 09:27:51 +0800

    add pic detial toolbar

--- app/src/main/java/cc/zkteam/juediqiusheng/module/pic/details/JDQSSeeBigPicActivity.kt
+++ app/src/main/java/cc/zkteam/juediqiusheng/module/pic/details/JDQSSeeBigPicActivity.kt
@@ -3,6 +3,8 @@ package cc.zkteam.juediqiusheng.module.pic.details
 import android.graphics.drawable.Animatable
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.support.v7.widget.Toolbar
+import android.view.Window
 import cc.zkteam.juediqiusheng.R
 import cc.zkteam.juediqiusheng.utils.L
 import com.alibaba.android.arouter.facade.annotation.Autowired
@@ -27,17 +29,20 @@ class JDQSSeeBigPicActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        supportRequestWindowFeature(Window.FEATURE_NO_TITLE)
         setContentView(R.layout.activity_see_big_pic_layout)
         ARouter.getInstance().inject(this)
 
-        L.i("当前的 Url是：" + url)
+        val toolbar: Toolbar = findViewById(R.id.toolbar_pic_detail)
+        setSupportActionBar(toolbar)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
 
-//        添加 https://github.com/ongakuer/PhotoDraweeView
+        L.i("当前的 Url是：" + url)
 
         val controller = Fresco.newDraweeControllerBuilder()
         controller.setUri(url)
-        controller.oldController = see_big_pic_imageView.getController()
-        controller.setControllerListener(object : BaseControllerListener<ImageInfo>() {
+        controller.oldController = see_big_pic_imageView.controller
+        controller.controllerListener = object : BaseControllerListener<ImageInfo>() {
             override fun onFinalImageSet(id: String?, imageInfo: ImageInfo?, animatable: Animatable?) {
                 super.onFinalImageSet(id, imageInfo, animatable)
                 if (imageInfo == null || see_big_pic_imageView == null) {
@@ -45,8 +50,8 @@ class JDQSSeeBigPicActivity : AppCompatActivity() {
                 }
                 see_big_pic_imageView.update(imageInfo.width, imageInfo.height)
             }
-        })
-        see_big_pic_imageView.setController(controller.build())
+        }
+        see_big_pic_imageView.controller = controller.build()
 
     }
 }
