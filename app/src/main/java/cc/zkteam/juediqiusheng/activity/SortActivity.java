60ec3491516d22a6a5a026e347db6ef9ee2954ac
qingw <qingw@jumei.com>
2017-12-18 16:34:50 +0800

    1. 攻略页面点击事件添加。

--- app/src/main/java/cc/zkteam/juediqiusheng/activity/SortActivity.java
+++ app/src/main/java/cc/zkteam/juediqiusheng/activity/SortActivity.java
@@ -1,20 +1,25 @@
 package cc.zkteam.juediqiusheng.activity;
 
 import android.content.Intent;
-import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.LinearLayoutManager;
 import android.text.TextUtils;
 import android.util.Log;
+import android.view.View;
+
+import com.chad.library.adapter.base.BaseQuickAdapter;
 
 import java.util.List;
 
 import cc.zkteam.juediqiusheng.R;
+import cc.zkteam.juediqiusheng.adapter.SortAdapter;
 import cc.zkteam.juediqiusheng.bean.SortDetailBean;
 import cc.zkteam.juediqiusheng.managers.ZKConnectionManager;
 import cc.zkteam.juediqiusheng.retrofit2.ZKCallback;
+import cc.zkteam.juediqiusheng.view.ZKRecyclerView;
 
 public class SortActivity extends BaseActivity {
     private static final String TAG = "sort";
-    private RecyclerView rvSort;
+    private ZKRecyclerView zkRecyclerView;
     private String jid;
 
     @Override
@@ -24,13 +29,11 @@ public class SortActivity extends BaseActivity {
 
     @Override
     protected void initViews() {
-        rvSort = findViewById(R.id.zk_recycler_view);
-
+        zkRecyclerView = findViewById(R.id.zk_recycler_view);
     }
 
     @Override
     protected void initListener() {
-
         Intent intent = getIntent();
         if (intent != null){
             jid = intent.getStringExtra("id");
@@ -40,9 +43,6 @@ public class SortActivity extends BaseActivity {
             else
                 setTitle("分类详情");
         }
-
-
-
     }
 
     @Override
@@ -55,21 +55,21 @@ public class SortActivity extends BaseActivity {
                 .enqueue(new ZKCallback<List<SortDetailBean>>() {
                     @Override
                     public void onResponse(final List<SortDetailBean> result) {
-//                        SortAdapter sortAdapter = new SortAdapter(mContext, result, new RvListener() {
-//                            @Override
-//                            public void onItemClick(int id, int position) {
-//                                Intent intent = new Intent();
-//                                intent.setClass(mContext, WebViewActivity.class);
-//                                String artifactUrl = result.get(position).getArtifactUrl();
-//                                String url = artifactUrl.substring(artifactUrl.lastIndexOf("\">") + 2, artifactUrl.lastIndexOf("</a>"));
-//                                intent.putExtra("url", url);
-//                                startActivity(intent);
-//
-//                            }
-//                        });
-//                        rvSort.setLayoutManager(new LinearLayoutManager(mContext));
-//                        rvSort.setAdapter(sortAdapter);
-//                        Log.d(TAG, "onResponse() called with: resultList = [" + result + "]");
+                        SortAdapter sortAdapter = new SortAdapter(result);
+                        sortAdapter.setOnItemClickListener(new BaseQuickAdapter.OnItemClickListener() {
+                            @Override
+                            public void onItemClick(BaseQuickAdapter adapter, View view, int position) {
+                                Intent intent = new Intent();
+                                intent.setClass(mContext, WebViewActivity.class);
+                                String artifactUrl = result.get(position).getArtifactUrl();
+                                String url = artifactUrl.substring(artifactUrl.lastIndexOf("\">") + 2, artifactUrl.lastIndexOf("</a>"));
+                                intent.putExtra("url", url);
+                                startActivity(intent);
+                            }
+                        });
+                        zkRecyclerView.setLayoutManager(new LinearLayoutManager(mContext));
+                        zkRecyclerView.setAdapter(sortAdapter);
+                        Log.d(TAG, "onResponse() called with: resultList = [" + result + "]");
                     }
 
                     @Override
