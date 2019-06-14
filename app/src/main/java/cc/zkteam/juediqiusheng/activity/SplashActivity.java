bf573a5295016c7b768a4f8408c334bb81e3bd1b
WangQing <wangqing11@100tal.com>
2019-06-15 04:03:58 +0800

    1. 修复 头条启动权限问题。可能还需要微调。

--- app/src/main/java/cc/zkteam/juediqiusheng/activity/SplashActivity.java
+++ app/src/main/java/cc/zkteam/juediqiusheng/activity/SplashActivity.java
@@ -33,10 +33,12 @@ public class SplashActivity extends BaseActivity {
     private static final int FLAG_ENTER_MAIN =  0;
     private static final int FLAG_REQUEST_PERMISSION =  100;
 
-
-    private String[] permissions = new String[]{Manifest.permission.READ_PHONE_STATE,
+    private String[] permissions = new String[]{
+            Manifest.permission.READ_PHONE_STATE,
             Manifest.permission.READ_EXTERNAL_STORAGE,
-            Manifest.permission.WRITE_EXTERNAL_STORAGE};
+            Manifest.permission.ACCESS_COARSE_LOCATION,
+            Manifest.permission.WRITE_EXTERNAL_STORAGE
+    };
 
 
     @SuppressLint("HandlerLeak")
@@ -62,17 +64,20 @@ public class SplashActivity extends BaseActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 //        setContentView(R.layout.activity_splash);
+        permissionCheck();
         initView();
         initData();
         initAnimation();
+    }
 
+    private void showKPAD() {
         //开屏广告
         String kpId = "818599619";
 //        if (!BuildConfig.DEBUG) {
 //            kpId = "820203984"; //正式 key
 //        }
 
-        ZKAD.showTouTiaoSplashAD(kpId, this, ad -> splashHandler.sendEmptyMessage(FLAG_ENTER_MAIN));
+        ZKAD.showTouTiaoSplashAD(kpId, this, ad -> splashHandler.sendEmptyMessageDelayed(FLAG_ENTER_MAIN, DELAY_TIME));
     }
 
     @Override
@@ -106,8 +111,6 @@ public class SplashActivity extends BaseActivity {
     }
 
     private void initAnimation() {
-        permissionCheck();
-
         WindowManager wm = (WindowManager)
                 getSystemService(this.WINDOW_SERVICE);
 
@@ -160,14 +163,17 @@ public class SplashActivity extends BaseActivity {
                 .callback(new PermissionUtils.FullCallback() {
                     @Override
                     public void onGranted(List<String> permissionsGranted) {
-//                        splashHandler.sendEmptyMessageDelayed(FLAG_ENTER_MAIN, DELAY_TIME);
+                        showKPAD();
                     }
 
                     @Override
                     public void onDenied(List<String> permissionsDeniedForever, List<String> permissionsDenied) {
                         ToastUtils.showShort(getString(R.string.question_permission_tip));
-//                        splashHandler.sendEmptyMessageDelayed(FLAG_ENTER_MAIN, DELAY_TIME);
+                        splashHandler.sendEmptyMessageDelayed(FLAG_ENTER_MAIN, DELAY_TIME);
                     }
+                })
+                .rationale(shouldRequest -> {
+                    showKPAD();
                 })
                 .request();
 
